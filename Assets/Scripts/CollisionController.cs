8b2ed4fb961441e53708d047bf78c735f5c8a750
liamftw <53338463+liamftw@users.noreply.github.com>
2024-06-18 16:27:01 +0100

    The health update
    The update what adds a functional player health mechanic, some audio changes, UI bar added with blinking player icon

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollisionController : MonoBehaviour
 {
     public AudioSource source;
     public AudioClip clip;
+    private playerHealth playerHealthScript;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    void Start()
+    {
+        playerHealthScript = FindObjectOfType<playerHealth>();
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.name == "Player")
         {
             Debug.Log("HIT!");
 
-            // Play the audio clip
+            // script to take away 1 heart after a collision
+            if (playerHealthScript != null)
+            {
+                playerHealthScript.clydeHealth -= 1;
+                // make sure that the health doesnt go below zero
+                if (playerHealthScript.clydeHealth < 0)
+                {
+                    playerHealthScript.clydeHealth = 0;
+                }
+            }
+
+            // play the audio clip
             if (source != null && clip != null)
             {
                 source.PlayOneShot(clip);
---
+++ Assets/Scripts/playerHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class playerHealth : MonoBehaviour
+{
+    public int clydeHealth;
+    public int clydeHeartNum;
+
+    public Image[] clydeHearts;
+    public Sprite fullHeart;
+    public Sprite emptyHeart;
+
+    void Update()
+    {
+        if (clydeHealth > clydeHeartNum)
+        {
+            clydeHealth = clydeHeartNum;
+        }
+
+        if (clydeHealth == 0)
+        {
+            SceneManager.LoadScene("Menu");
+        }
+
+        for (int i = 0; i < clydeHearts.Length; i++)
+        {
+            if (i < clydeHealth)
+            {
+                clydeHearts[i].sprite = fullHeart;
+                clydeHearts[i].enabled = true;
+            }
+            else
+            {
+                clydeHearts[i].sprite = emptyHeart;
+                clydeHearts[i].enabled = false;
+            }
+        }
+    }
+}
